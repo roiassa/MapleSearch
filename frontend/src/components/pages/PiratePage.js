d1fea5d176a6e376af1b68ec6a410a3fce4fe804
Roy <roymeplease@gmail.com>
2020-05-06 23:55:35 +0300

    PiratePage component for the Pages component

---
+++ frontend/src/components/pages/PiratePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PirateImg from '../../assets/img-src/386px-Pirate_Female.png'
+
+function PiratePage() {
+    return (
+        <React.Fragment>
+            <h1 className="job-title">Pirate</h1>
+            <img src={PirateImg} alt="Pirate"></img>
+            <div className="job-overview">
+                <p>Ahoy, mateys! If it's adventure ye be seeking, then the Pirate is the class for you!
+                     These swashbuckling rogues come equipped with guns and brass knuckles, and can specialize as either a Buccaneer or Corsair depending on their preference.
+                      Buccaneers can channel their inner energy to deliver powerful strikes. Corsairs are masters of firearms and offer a variety of rapid-fire,
+                       high caliber attacks.</p>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default PiratePage
