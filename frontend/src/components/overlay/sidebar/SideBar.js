fcaef03a1cb9a78925f562907bd17141b130c32f
Roy <roymeplease@gmail.com>
2020-05-06 14:37:32 +0300

    SideBar component added

---
+++ frontend/src/components/overlay/sidebar/SideBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserSecret, faHome } from '@fortawesome/free-solid-svg-icons'
+
+
+function SideBar() {
+    return (
+        <nav className="side-bar">
+            <Link to="/" ><div className="link-div">
+                <FontAwesomeIcon icon={faHome} className="icon-home" />
+                <span className="nav-link">Home</span>
+            </div>
+            </Link>
+            <Link to="/about" ><div className="link-div">
+                <FontAwesomeIcon icon={faUserSecret} className="icon-user" />
+                <span className="nav-link">About</span>
+            </div>
+            </Link>
+        </nav>
+    )
+}
+
+
+export default SideBar;
