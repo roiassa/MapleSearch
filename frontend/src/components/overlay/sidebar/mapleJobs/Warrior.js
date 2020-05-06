27d0660fa42d0dc1d7dad26ef970bd5357c596f5
Roy <roymeplease@gmail.com>
2020-05-06 14:43:59 +0300

    minor Warrior component fix

--- frontend/src/components/overlay/sidebar/mapleJobs/Warrior.js
+++ frontend/src/components/overlay/sidebar/mapleJobs/Warrior.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserSecret, faHome } from '@fortawesome/free-solid-svg-icons'
 
 function Warrior() {
     return (
