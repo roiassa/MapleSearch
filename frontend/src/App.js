2d60855719794f24535ed56085125f1ab1faa87d
Roy <roymeplease@gmail.com>
2020-05-06 14:43:40 +0300

    added SideBar to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/overlay/header/Header'
+import SideBar from './components/overlay/sidebar/SideBar'
 import BackgroundImg from './components/overlay/BackgroundImg'
 import './css/dist/App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -12,6 +13,7 @@ function App() {
       <React.Fragment>
         <BackgroundImg />
         <Header />
+        <SideBar />
       </React.Fragment>
     </Router>
   );
