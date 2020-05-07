a1da57e13cb24681513a9bf486522aa13f9b2ea1
Roy <roymeplease@gmail.com>
2020-05-08 02:00:13 +0300

    changed the logo of the Bowman component in the sidebar

--- frontend/src/components/overlay/sidebar/mapleJobs/Bowman.js
+++ frontend/src/components/overlay/sidebar/mapleJobs/Bowman.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRandom } from '@fortawesome/free-solid-svg-icons'
+import { faBullseye } from '@fortawesome/free-solid-svg-icons'
 
 function Bowman() {
     return (
         <Link to="/bowman" ><div className="link-div">
-            <FontAwesomeIcon icon={faRandom} className="sidebar-icon" />
+            <FontAwesomeIcon icon={faBullseye} className="sidebar-icon" />
             <span className="nav-link2">Bowman</span>
         </div>
         </Link>
