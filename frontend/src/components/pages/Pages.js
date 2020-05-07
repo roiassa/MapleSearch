daf3092909a4dc2e13bfaf297c4d5dff9a2b4f90
Roy <roymeplease@gmail.com>
2020-05-07 17:01:17 +0300

    Pages component customization changes

--- frontend/src/components/pages/Pages.js
+++ frontend/src/components/pages/Pages.js
@@ -4,19 +4,26 @@ import MagicianPage from './MagicianPage'
 import ThiefPage from './ThiefPage'
 import BowmanPage from './BowmanPage'
 import PiratePage from './PiratePage'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import corners from '../../assets/img-src/kisspng-borders-and-frames-gold-clip-art-gold-corner-5ab9582ea29f60.5564116915220961746661.png'
+import { BrowserRouter as Router, Route, } from 'react-router-dom'
 
 function Pages() {
     return (
-            <div className="wrapper">
-                <div className="pages-window">
-                        <Route exact path="/warrior" component={WarriorPage}></Route>
-                        <Route exact path="/magician" component={MagicianPage}></Route>
-                        <Route exact path="/thief" component={ThiefPage}></Route>
-                        <Route exact path="/bowman" component={BowmanPage}></Route>
-                        <Route exact path="/pirate" component={PiratePage}></Route>
-                </div>
+        <div className="wrapper">
+            {/* <div className="corner-div">
+                    <img src={corners} className="left-top"></img>
+                    <img src={corners} className="left-bottom"></img>
+                    <img src={corners} className="right-top"></img>
+                    <img src={corners} className="right-bottom"></img>
+                </div> */}
+            <div className="pages-window">
+                <Route exact path="/warrior" component={WarriorPage}></Route>
+                <Route exact path="/magician" component={MagicianPage}></Route>
+                <Route exact path="/thief" component={ThiefPage}></Route>
+                <Route exact path="/bowman" component={BowmanPage}></Route>
+                <Route exact path="/pirate" component={PiratePage}></Route>
             </div>
+        </div>
     )
 }
 
