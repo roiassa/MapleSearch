d1e3bee975590c7565f0ee9d8c1c27d1aab5a99d
Roy <roymeplease@gmail.com>
2020-05-07 19:52:22 +0300

    new logo and customization

--- frontend/src/components/overlay/header/Header.js
+++ frontend/src/components/overlay/header/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import HeaderImg from './HeaderImg'
-import { Link } from 'react-router-dom'
+
 
 function Header() {
 
     return (
         <div className="header">
-            <Link to="/">
                 <HeaderImg />
-            </Link>
         </div>
     )
 }
--- frontend/src/components/overlay/header/HeaderImg.js
+++ frontend/src/components/overlay/header/HeaderImg.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import mushroom from '../../../assets/img-src/kisspng-maplestory-jungle-adventures-super-story-video-ga-5af6c8acb28589.9486227815261226687312.png'
 import mapleTree from '../../../assets/img-src/kisspng-maplestory-skill-computer-software-game-quest-censored-png-5b56ad53211b78.1610534215324071231356.png'
+import Logo from '../../../assets/img-src/logo_transparent.png'
+import { Link } from 'react-router-dom'
 
 function HeaderImg() {
     return (
-        <div className="logo-container">
-            <img src={mushroom} alt="):"></img>
-            <h1 className='title'>@MapleSearch</h1>
-            <img src={mapleTree} alt="):"></img>
-        </div>
+        <Link to="/" >
+            <div className="logo-container">
+                {/* <img src={mushroom} alt="):"></img> */}
+                {/* <h1 className='title'>@MapleSearch</h1> */}
+                {/* <img src={mapleTree} alt="):"></img> */}
+                <img src={Logo} alt="Logo"></img>
+            </div>
+        </Link>
     )
 }
 
