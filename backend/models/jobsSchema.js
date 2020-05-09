b09fbea1dc10646436c54e3241134ef629ea0e59
Roy <roymeplease@gmail.com>
2020-05-09 15:54:29 +0300

    new jobs schema

---
+++ backend/models/jobsSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const JobSchema = new Schema({
+    path: String,
+    title: String,
+    image: Buffer,
+    overview: String
+});
+
+const Job = mongoose.model('job', JobSchema);
+
+
+module.exports = Job
