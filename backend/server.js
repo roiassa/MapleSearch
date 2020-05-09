9e06b0b2f3437a5b69f0055c97a4cfe118cabd2e
Roy <roymeplease@gmail.com>
2020-05-09 17:19:39 +0300

    now passing the uploades folder as a publicly available folder

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,10 @@ const app = express();
 
 mongoose.connect('mongodb://localhost/maplesearch', {useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 
+//making the uploades folder publicly available
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.json());
-
-// app.use('/api', require('./routes/api'));
+app.use('/api', require('./routes/jobs'));
 
 app.use(function(err, req, res, next){
     res.status(422).send({error: err.message})
