624c0df16c6c95c2d250e65fedf073c40304ba63
Roy <roymeplease@gmail.com>
2020-05-09 19:57:16 +0300

    all the job pages data now comes from the database and not from different components

--- frontend/src/components/pages/Pages.js
+++ frontend/src/components/pages/Pages.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import HomePage from './HomePage'
 // import corners from '../../assets/img-src/kisspng-borders-and-frames-gold-clip-art-gold-corner-5ab9582ea29f60.5564116915220961746661.png'
 import { BrowserRouter as Router, Route, } from 'react-router-dom'
-import data from './pagesData'
+
 
 function Pages() {
+
+    const [jobPages, setJobPages] = useState([])
+
+    useEffect(() => {
+        const url = '/api/jobs'
+        axios.get(url)
+        .then(res => {
+            setJobPages(res.data)
+            
+        })
+        .catch(err => {
+            if(err.reponse.status === 404) {
+                console.log(err)
+            }
+        })
+    }, [])
+
     return (
         <div className="wrapper">
             {/* <div className="corner-div">
@@ -15,11 +33,11 @@ function Pages() {
                 </div> */}
             <div className="pages-window">
                  <Route exact path="/" component={HomePage} />
-                {data.map((page, index) => {
+                {jobPages.map((page, index) => {
                     return <Route exact path={page.path} key={index}>
                         <React.Fragment>
                             <h1 className="title">{page.title}</h1>
-                            <img src={page.img} alt='):'></img>
+                            <img src={page.image} alt='):'></img>
                             <p className="overview">{page.overview}</p>
                         </React.Fragment>
                     </Route>
