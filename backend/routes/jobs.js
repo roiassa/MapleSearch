1bc5b208d8a18f4dd8a5ff73afcc1b47d83ab8a3
Roy <roymeplease@gmail.com>
2020-05-09 17:20:27 +0300

    with multer, now uploading files is possible

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const Job = require('../models/jobsSchema')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        //null if there's an error and path where we store the file:
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        //the date that we uploaded the file(replacing the ":" with "-" because of windows problem) with the originalname of the file:
+        cb(null, new Date().toISOString().replace(/:/g, '-')+ file.originalname);
+    }
+})
+
+
+//rejecting a file if it's not the type of file we want:
+const fileFilter = (req, file,cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        cb(new Error('Type file is not supported!'), false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter
+});
 
 
 router.get('/jobs', function(req, res){
@@ -10,11 +37,12 @@ router.get('/jobs', function(req, res){
 })
 
 
-router.post('/jobs', function(req, res) {
+router.post('/jobs', upload.single('jobImage'), function(req, res) {
+    console.log(req.file.path)
     Job.create({
         path: req.body.path,
         title: req.body.title,
-        image: req.body.image,
+        image: req.file.path,
         overview: req.body.overview
     }).then(function(job){
         res.send(job)
