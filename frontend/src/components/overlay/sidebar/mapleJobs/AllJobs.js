ca9a99f172820280ff303668e8259fdbb38f6000
Roy <roymeplease@gmail.com>
2020-05-10 00:33:54 +0300

    the job list for the sidebar now is now in a single file (allJobsData.js) instead of each job having it's own component

--- frontend/src/components/overlay/sidebar/mapleJobs/AllJobs.js
+++ frontend/src/components/overlay/sidebar/mapleJobs/AllJobs.js
@@ -2,45 +2,35 @@ import React from 'react'
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import Warrior from './Warrior'
-import Magician from './Magician'
-import Thief from './Thief'
-import Bowman from './Bowman'
-import Pirate from './Pirate'
+import jobList from './AllJobsData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom';
+
 
 function AllJobs() {
     return (
         <Accordion>
-            <Card style={{backgroundColor: '#F4511E', }}>
-                <Card.Header style={{padding: '0'}}>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0" style={{color: 'white', width: '100%', padding: '0'}}>
+            <Card style={{ backgroundColor: '#F4511E', }}>
+                <Card.Header style={{ padding: '0' }}>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0" style={{ color: 'white', width: '100%', padding: '0' }}>
                         Jobs
                     </Accordion.Toggle>
                 </Card.Header>
 
                 <Accordion.Collapse eventKey="0">
-                    <Card.Body style={{padding: '0'}}><Warrior /></Card.Body>
-                </Accordion.Collapse>
-
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body style={{padding: '0'}}><Magician /></Card.Body>
-                </Accordion.Collapse>
-
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body style={{padding: '0'}}><Thief /></Card.Body>
-                </Accordion.Collapse>
-
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body style={{padding: '0'}}><Bowman /></Card.Body>
-                </Accordion.Collapse>
-
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body style={{padding: '0'}}><Pirate /></Card.Body>
+                    <Card.Body style={{ padding: '0' }}>
+                        {jobList.map((job, index) => {
+                            return <Link to={job.link} key={index}><div className="link-div">
+                                <FontAwesomeIcon icon={job.icon} className="sidebar-icon" />
+                                <span className="nav-link">{job.title}</span>
+                            </div>
+                        </Link>
+                        })}
+                    </Card.Body>
                 </Accordion.Collapse>
             </Card>
-
         </Accordion>
     )
 }
 
-export default AllJobs
+export default AllJobs;
---
+++ frontend/src/components/overlay/sidebar/mapleJobs/AllJobsData.js
@@ -0,0 +1,33 @@
+import { faFistRaised, faMagic, faDollarSign, faBullseye, faSkullCrossbones } from '@fortawesome/free-solid-svg-icons'
+
+
+const jobList = [
+    {
+        link: "/warrior",
+        icon: faFistRaised, 
+        title: "Warrior"
+    },
+    {
+        link: "/magician",
+        icon: faMagic,
+        title: "Magician"
+    },
+    {
+        link: "/thief",
+        icon: faDollarSign,
+        title: "Thief"
+    },
+    {
+        link: "/bowman",
+        icon: faBullseye,
+        title: "Bowman"
+    },
+    {
+        link: "/pirate",
+        icon: faSkullCrossbones,
+        title: "Pirate"
+    }
+]
+
+
+export default jobList;
