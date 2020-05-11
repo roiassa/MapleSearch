800a1f6d7e7a8caf9509f908cc985724a7e41519
Roy <roymeplease@gmail.com>
2020-05-12 01:28:19 +0300

    added an error if the server returns undefined

--- frontend/src/helpers/apicalls/searchJobsApi.js
+++ frontend/src/helpers/apicalls/searchJobsApi.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
 
-export default function searchJobsGet(callBack,value,isCash, error) {
+export default function searchJobsGet(callBack, value, isCash, error) {
     const url = `https://maplestory.io/api/gms/213/item/list?jobFilter=${value}&cashFilter=${isCash}`
     axios.get(url)
         .then(res => {
             callBack(res.data)
         })
         .catch(err => {
-            if (err.reponse.status === 404) {
+            if (err.respone === undefined) {
+                alert("Api is down")
+            }
+            else if (err.reponse.status === 404) {
                 console.log(err)
             }
         })
