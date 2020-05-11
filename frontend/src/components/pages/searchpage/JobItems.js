c7edb4809f97c23639c0d595ce57c22a72f5802c
Roy <roymeplease@gmail.com>
2020-05-11 19:04:28 +0300

    item list custimzation

--- frontend/src/components/pages/searchpage/JobItems.js
+++ frontend/src/components/pages/searchpage/JobItems.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 function JobItems(props) {
     return (
-        <div>
+        <React.Fragment>
             {props.items.map((item, index) => {
-                return <React.Fragment key={index}>
-                    <h1 >Item: {item.name}</h1>
-                    <h1>Required Jobs: {item.requiredJobs}</h1>
-                    <h1>Required Level: {item.requiredLevel}</h1>
-                    <h1>Category: {item.typeInfo.subCategory}</h1>
-                    <img src={`https://maplestory.io/api/gms/213/item/${item.id}/icon`} style={{ height: '50px' }}></img>
-                </React.Fragment>
+                return <div className='item' key={index}>
+                    <div className='item-name'>
+                        <span>{item.name}</span>
+                    </div>
+                    <div className='item-details'>
+                        <p>Required Jobs: {item.requiredJobs}</p>
+                        <p>Required Level: {item.requiredLevel}</p>
+                        <p>Category: {item.typeInfo.subCategory}</p>
+                    </div>
+                    <div className="item-img">
+                        <img src={`https://maplestory.io/api/gms/213/item/${item.id}/icon`}></img>
+                    </div>
+                </div>
             })
             }
-        </div>
+        </React.Fragment>
     )
 }
 
