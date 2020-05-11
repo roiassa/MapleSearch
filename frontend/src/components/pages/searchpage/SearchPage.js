738a0d80b3f3f7af03a371a2ba9996c91f7de239
Roy <roymeplease@gmail.com>
2020-05-11 03:51:53 +0300

    all items are now imported from the JobItems component

--- frontend/src/components/pages/searchpage/SearchPage.js
+++ frontend/src/components/pages/searchpage/SearchPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import DropdownJobMenu from './DropDownJobMenu'
+import JobItems from './JobItems'
 
 
 function SearchPage() {
@@ -12,18 +13,9 @@ function SearchPage() {
             <DropdownJobMenu 
             setItems={setItems}
             />
-            <div>
-                {items.map((item, index) => {
-                    return <React.Fragment key={index}>
-                        <h1 >Item: {item.name}</h1>
-                        <h1>Required Jobs: {item.requiredJobs}</h1>
-                        <h1>Required Level: {item.requiredLevel}</h1>
-                        <h1>Category: {item.typeInfo.subCategory}</h1>
-                        <img src={`https://maplestory.io/api/gms/213/item/${item.id}/icon`} style={{height: '50px'}}></img>
-                    </React.Fragment>
-                })
-                } 
-            </div>
+            <JobItems 
+            items={items}
+            />
         </React.Fragment>
     )
 }
