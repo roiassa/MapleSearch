165d43c0b203b326d337f17101e261d25645867a
Roy <roymeplease@gmail.com>
2020-05-12 05:15:41 +0300

    added a image to loading page and a span Loading...

--- frontend/src/components/pages/LoadingPage.js
+++ frontend/src/components/pages/LoadingPage.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Spinner from 'react-bootstrap/Spinner'
+import pinkBean from '../../assets/img-src/Pink Bean.png'
 
 function LoadingPage(props) {
     if (props.isLoading) {
         setTimeout(() => {
             props.setIsLoading(false)
-        }, 2000)
+        }, 3000)
     }
     return (
         <React.Fragment>
             {props.isLoading ? <div className="loading-page">
+                <span>Loading...</span>
                 <Spinner animation="grow" variant="primary" />
+                <img src={pinkBean}></img>
             </div> : null}
         </React.Fragment>
     )
