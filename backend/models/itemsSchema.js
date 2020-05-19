0dda26f758474c528e99d5bef2e746109bc5eca9
Roy <roymeplease@gmail.com>
2020-05-19 19:58:33 +0300

    added an id property to ItemSchema

--- backend/models/itemsSchema.js
+++ backend/models/itemsSchema.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
+    id: String,
     name: String,
     requiredJobs: String,
     requiredLevels: String,
