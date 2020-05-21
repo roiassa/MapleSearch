4b744c89f04d028521b1dfa393938896a324203e
Roy <roymeplease@gmail.com>
2020-05-21 04:02:18 +0300

    original get route is now set to search for all job items if they are cash or not and a new route has been added to get items for a specific job with the cash filter

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const Item = require('../models/itemsSchema');
 
 
-router.get('/items', function (req, res, next) {
-    Item.find({})
+router.get('/items/:isCash', function (req, res, next) {
+    Item.find({isCash: req.params.isCash})
         .then((item) => {
             res.send(item)
         })
 
+        .catch(next)
+})
+
+router.get('/items/:requiredJobs/:isCash', function (req, res, next) {
+    Item.find({requiredJobs: req.params.requiredJobs, isCash: req.params.isCash})
+        .then((item) => {
+            res.send(item)
+        })
         .catch(next)
 })
 
