4d8af04f4fdb6bcd18bdca9edfd09dffcd0eac5d
Roy <roymeplease@gmail.com>
2020-05-22 04:36:08 +0300

    now the eventKey for each dropdown item is the job's name instead of the value

--- frontend/src/components/pages/searchpage/DropDownJobMenu.js
+++ frontend/src/components/pages/searchpage/DropDownJobMenu.js
@@ -5,18 +5,21 @@ import dropDownJobs from '../../../helpers/dropDownJobs'
 import searchJobsApi from '../../../helpers/apicalls/searchJobsApi'
 
 function DropDown(props) {
-
+    
     const handleSelect = (value) => {
         searchJobsApi(props.setItems, value, props.isCash)
         props.setIsLoading(!props.isLoading)
-        props.setActiveSearch(true)
+        props.setActivePerJobSearch(true)
+        props.setCurrentPage(1)
+        props.setSearch('')
     }
+
 
     return (
         <React.Fragment>
             <DropdownButton title='Choose a job' onSelect={(eventKey) => { handleSelect(eventKey) }}>
                 {dropDownJobs.map((job, index) => {
-                    return <Dropdown.Item eventKey={job.value} key={index}>{job.name}</Dropdown.Item>
+                    return <Dropdown.Item eventKey={job.name} key={index}>{job.name}</Dropdown.Item>
                 })}
             </DropdownButton>{' '}
         </React.Fragment>
