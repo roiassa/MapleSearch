e1087842a1c696eaf50bd8da0dcc2a4e38486ca0
Roy <roymeplease@gmail.com>
2020-05-22 04:34:32 +0300

    New AllItems component. Now all items from the game, no matter the job, is shows in this component

---
+++ frontend/src/components/pages/searchpage/AllItems.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import getAllItems from '../../../helpers/apicalls/allItemsApi'
+
+function AllItems(props) {
+
+    useEffect(() => {
+        getAllItems(props.isCash, props.setAllItems)
+    }, [props.isCash, props.setAllItems])
+
+    //Get current items
+    const indexOfLastItem = props.currentPage * props.itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - props.itemsPerPage
+    const currentItems = props.allItems.slice(indexOfFirstItem, indexOfLastItem)
+
+    return (
+        <React.Fragment>
+            {currentItems.map((item, index) => {
+                return <div className='item' key={index}>
+                    <div className='item-name'>
+                        <span>{item.name}</span>
+                    </div>
+                    <div className='item-details'>
+                        <p>Required Jobs: {item.requiredJobs}</p>
+                        <p>Required Level: {item.requiredLevel}</p>
+                        <p>Category: {item.category}</p>
+                    </div>
+                    <div className="item-img">
+                        <img src={item.image} alt="):"></img>
+                    </div>
+                </div>
+            })
+            }
+        </React.Fragment>
+    )
+}
+
+export default AllItems
