34ac9cb983d4202851c011e30e58791d1a1030eb
Roy <roymeplease@gmail.com>
2020-05-24 08:56:52 +0300

    New state currentItems. Will contain the current items of each items list when seperated to pages

--- frontend/src/components/pages/searchpage/SearchPage.js
+++ frontend/src/components/pages/searchpage/SearchPage.js
@@ -12,10 +12,10 @@ function SearchPage() {
     const [isCash, setIsCash] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
     const [currentPage, setCurrentPage] = useState(1)
+    const [currentItems, setCurrentItems] = useState([])
     const [itemsPerPage] = useState(10)
     const [activePerJobSearch, setActivePerJobSearch] = useState(false)
     const [inputActiveSearch, setInputActiveSearch] = useState(false)
-
 
     return (
         <React.Fragment>
@@ -50,6 +50,8 @@ function SearchPage() {
                 itemsPerPage={itemsPerPage}
                 currentPage={currentPage}
                 setCurrentPage={setCurrentPage}
+                currentItems={currentItems}
+                setCurrentItems={setCurrentItems}
                 isCash={isCash}
                 inputActiveSearch={inputActiveSearch}
                 activePerJobSearch={activePerJobSearch}
