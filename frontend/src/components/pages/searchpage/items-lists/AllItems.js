299304ec580a0bc8c49af7530fcb78ce8a3dc59b
Roy <roymeplease@gmail.com>
2020-05-24 09:02:42 +0300

    Api call is now wrapped in react's useCallback hook to avoid useEffect dependecy warnings. Also, calling the getCurrentPageItems helper function that takes care of that logic there instead of inside this component, also wrapped in react's useCallback for the same reasons.

--- frontend/src/components/pages/searchpage/items-lists/AllItems.js
+++ frontend/src/components/pages/searchpage/items-lists/AllItems.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import getAllItems from '../../../../helpers/apicalls/allItemsApi'
+import getCurrentPageItems from '../../../../helpers/getCurrentPageItems'
 
 
 function AllItems(props) {
 
-    useEffect(() => {
+    //wrapping the functions in react's useCallback hook to avoid useEffect dependency warnings:
+    const getAllItemsMemo = useCallback(() => {
         getAllItems(props.isCash, props.setAllItems)
     }, [props.isCash, props.setAllItems])
 
-    //Get current items
-    const indexOfLastItem = props.currentPage * props.itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - props.itemsPerPage
-    const currentItems = props.allItems.slice(indexOfFirstItem, indexOfLastItem)
+    const getCurrentPageItemsMemo = useCallback(() => {
+        getCurrentPageItems(props.currentPage, props.itemsPerPage, props.allItems, props.setCurrentItems)
+    }, [props.currentPage, props.itemsPerPage, props.allItems, props.setCurrentItems])
+
+    useEffect(() => {
+        getAllItemsMemo()
+    }, [getAllItemsMemo])
+
+    useEffect(() => {
+        getCurrentPageItemsMemo()
+    }, [getCurrentPageItemsMemo])
 
     return (
         <React.Fragment>
-            {currentItems.map((item, index) => {
+            {props.currentItems.map((item, index) => {
                 return <div className='item' key={index}>
                     <div className='item-name'>
                         <span>{item.name}</span>
