a0a0bff85a9d68b141a6ca52db971f6a520d7ff8
Roy <roymeplease@gmail.com>
2020-05-30 16:33:03 +0300

    heroku deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'});
 const bodyParser = require('body-parser');
-const connect = require('./db/mongodb')
+const connect = require('./db/mongodb');
+const path = require('path');
 
 const port = process.env.PORT || 3000
 
@@ -20,6 +21,19 @@ app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.json());
 app.use('/api', require('./routes/jobs'));
 app.use('/api', require('./routes/items'));
+
+// Serve static assets if in production:
+if(process.env.NODE_ENV === 'production') {
+    //Set a static folder:
+    app.use(express.static('frontend/build'))
+
+    //'*' will get any request that is not our routes written above
+    app.get('*', (req, res) => {
+        //
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
 
 app.use(function (err, req, res, next) {
     res.status(422).send({ error: err.message })
