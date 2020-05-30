bdaaea426d9787fbdcd8ee6c7079cf4a9f3830b2
Roy <roymeplease@gmail.com>
2020-05-30 17:56:34 +0300

    test

--- frontend/src/components/pages/Pages.js
+++ frontend/src/components/pages/Pages.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import jobGet from '../../helpers/apicalls/jobsApi.js'
 import HomePage from './HomePage'
+import jobGet from '../../helpers/apicalls/jobsApi'
 import JobPages from './JobPages'
 import SearchPage from './searchpage/SearchPage'
 import { BrowserRouter as Router, Route, } from 'react-router-dom'
