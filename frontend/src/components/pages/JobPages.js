a982ef5b30ae5a79fa6a805c792de0ef39ad9220
Roy <roymeplease@gmail.com>
2020-05-31 14:47:30 +0300

    addition of a go to search page button in the JobPages page.

--- frontend/src/components/pages/JobPages.js
+++ frontend/src/components/pages/JobPages.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function JobPages(props) {
     return (
@@ -7,6 +7,9 @@ function JobPages(props) {
             {props.jobPagesList.map((page, index) => {
                 return <Route exact path={page.path} key={index}>
                     <React.Fragment>
+                        <div className="go-to-search-wrapper">
+                        <Link to='/search' className='go-to-search-page'>Go to search page</Link>
+                        </div>
                         <h1 className="title">{page.title}</h1>
                         <img src={page.image} alt='):'></img>
                         <p className="overview">{page.overview}</p>
